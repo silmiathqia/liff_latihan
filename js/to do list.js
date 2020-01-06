ac157fa261e79daa2152c3139cee0e148f340e69
Silmi Azdkiatul Athqia <39125489+silmiathqia@users.noreply.github.com>
2020-01-06 19:58:13 +0700

    Update to do list.js
    sip

--- js/to do list.js
+++ js/to do list.js
@@ -1,126 +1,128 @@
-$(document).ready(function(){
-  var check_count = 0,
-      total = 0;
-  
-  //Custom Functions
-  function updateText(){
-    $('#count').text(total);
-    $('#count_done').text(check_count);
-    $('#remaining_done').text(total-check_count);
-  }
-  function showDate(){
-    var suffix = "", date = new Date(), dayOfMonth = date.getDate(), dayOfWeek = date.getDay(), Month = date.getMonth(),
-        $today =  $('#today'),
-        $daymonth =  $('#daymonth'),
-        $month =  $('#month');
+var listController = (function() {
+    counter = 3;
+    return {
+        addItem: function() {
+            counter += 1;
+        },
+        deleteItem: function(item) {
+            counter -= 1;
+        },
+        returnTotalItems: function() {
+            return counter;
+        },
+    }
+})();
+var UIController = (function() {
+    var DOMstrings = {
+        inputDescription: '.add__description',
+        inputBtn: '.add__btn',
+        todosContainer: '.todos__list',
+        container: '.container',
+        totalItems: '.total__items',
+        addField: '.add'
+    };
+    var items = listController.returnTotalItems();
+    return {
+        getInput: function() {
+            return document.querySelector(DOMstrings.inputDescription).value;
+        },
+        addListItem: function() {
+            var items = listController.returnTotalItems();
+            var html, newHtml, element;
+            element = DOMstrings.todosContainer;
+            var colors = ['#FFBE0B', '#FB5607', '#FF006E', '#8338EC', '#3A86FF'];
+            var gradients = [
+            'linear-gradient(to bottom, rgba(255,190,11, 1) 0%, rgba(255,190,11, 0.8) 100%)', 
+            'linear-gradient(to bottom, rgba(251, 86, 7, 1) 0%, rgba(251, 86, 7, 0.8) 100%)',
+            'linear-gradient(to bottom, rgba(255,0,110, 1) 0%, rgba(255,0,110, 0.8) 100%)',
+            'linear-gradient(to bottom, rgba(131, 56, 236, 1) 0%, rgba(131, 56, 236, 0.8) 100%)',
+            'linear-gradient(to bottom, rgba(58,134,255, 1) 0%, rgba(58,134,255, 0.8) 100%)'
+             ]
+            var random_gradient = gradients[Math.floor(Math.random() * gradients.length)];
+            html = '<div class="item" style="%style%"> <div class="item__description">%description%</div><div class="item__delete"><button class="item__delete--btn"><i class="ion-ios-trash-outline"></i></button></div></div>';
+            newHtml = html.replace('%description%', document.querySelector(DOMstrings.inputDescription).value);
+            newHtml = newHtml.replace('%style%', 'background:' + random_gradient) ;
+            document.querySelector(element).insertAdjacentHTML('beforeend', newHtml);
+        },
+        clearField: function() {
+            document.querySelector(DOMstrings.inputDescription).value = '';
+            document.querySelector(DOMstrings.inputDescription).focus();
+        },
+        displayTotalItems: function() {
+            var items = listController.returnTotalItems();
+            if (items == 0) {
+                document.querySelector(DOMstrings.totalItems).textContent = 'nothing';
+            } else if (items == 1) {
+                document.querySelector(DOMstrings.totalItems).textContent = ' 1 thing';
+            } else {
+                document.querySelector(DOMstrings.totalItems).textContent = items + ' things';
+            }
+        },
+        displayDate: function () {
+        	var mydate = new Date();
+    		var year = mydate.getYear();
+    		if (year < 1000) {
+        		year+=1900;
+    		}
+            var day = mydate.getDay();
+            var month = mydate.getMonth();
+            var daym = mydate.getDate();
+            if (daym < 10) {
+            	daym = "0" + daym;
+            }
+            var dayarray = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+            var montharray = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+            var today = dayarray[day] + " "+ daym + " "+ montharray[month] + " " + year;
+            document.getElementById('date').innerHTML = today;
+        },
 
-    var dayArray = ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"];
-    var monthArray = ["January","February","March","April,","May","June","July","August","September","October","November","December"];
-    
-    switch(dayOfMonth) {
-        case 1: case 21: case 31: suffix = 'st'; break;
-        case 2: case 22:          suffix = 'nd'; break;
-        case 3: case 23:          suffix = 'rd'; break;
-        default:                  suffix = 'th';
+        getDOMstrings: function() {
+            return DOMstrings;
+        },
+    };
+})();
+var controller = (function(listCtrl, UICtrl) {
+    var DOM = UICtrl.getDOMstrings();
+    var setupEventListeners = function() {
+   		document.querySelector(DOM.inputBtn).addEventListener('click', showAddField);
+        document.addEventListener('keypress', function(e) {
+            if (e.keyCode === 13 || e.which === 13) {
+                ctrlAddItem();
+            };
+        });
+        document.querySelector(DOM.container).addEventListener('click', ctrlDeleteItem);
+    };
+    var updateTotalItems = function() {
+        listController.returnTotalItems();
+        UICtrl.displayBudget(budget);
+    };
+    var ctrlAddItem = function() {
+        if (document.querySelector(DOM.inputDescription).value !== '') {
+            listCtrl.addItem();
+            UICtrl.addListItem();
+            UICtrl.clearField();
+            UICtrl.displayTotalItems();
+        };
+    };
+    var showAddField = function() {
+        document.querySelector(DOM.addField).style.display = 'block';
+        document.querySelector(DOM.inputBtn).addEventListener('click', ctrlAddItem)
+        document.querySelector(DOM.inputDescription).focus();
     }
-    
-    $today.text(dayArray[dayOfWeek] + ",");
-    $daymonth.text(" " + dayOfMonth + suffix);
-    $month.text(monthArray[Month]);
-  }
-  // Get the total number of "li's" and checked "li's"  
-  function loadLi(){
-    var findTheMarkedList = $('ul li');
-    for (var i = 0; i < findTheMarkedList.length; i++)
-    {
-      total++;
-      if ($(findTheMarkedList[i]).find('i').hasClass('fa-check-circle mark'))
-      {
-        $('li .right').eq(i).find('p').addClass('line-through').attr("contentEditable", false);
-        check_count++;
-      }
-    }
-    updateText();
-  }
-  
-  $('ul li').each(function(j){
-      var $this = $(this);
-      $('.bottom').addClass('show');
-      setTimeout(function () {
-        $this.addClass('down_in').removeClass('lihiden');
-         setTimeout(function () {
-            $this.removeAttr('class');
-         }, 550);
-      }, 60 * j );
-    });
-  
-  // Click on button function add new item to list
-  var li = "<li><a href='' class='check_button' onmousedown='return false'><i class='fa fa-circle-thin' aria-hidden='true'></i></a><div class='right'><p contenteditable='true'><strong></strong></p></div><span class='delete_button' onmousedown='return false'><i class='fa fa-times-circle' aria-hidden='true'></i></span></li>";
-  
-  $('.bottom #add-new').click(function(e){
-    e.preventDefault();
-    
-    $('ul').append(li).find('li:last-child').addClass('down');
-    
-    total += 1;
-    $('li:last-child p').text('New Task');
-
-    updateText();
-  });
-  // Click on button function list
-  $('.app ul').on('click','li .check_button',function(e){
-    e.preventDefault();
-    
-    let button = $(this).find('i');
-    let checked = 'fa fa-check-circle mark';
-    let unchecked = 'fa fa-circle-thin';
-    
-    // Save the current index of button after the click event in the "left" div.
-    let index_click = $('li .check_button').index(this);
-    // Use the current index of button to target the correct "li p" in the "right" div.
-    let linethrough_text = $('li .right').eq(index_click).find('p');
-    
-    if(button.hasClass(unchecked))
-    {
-      linethrough_text.addClass('line-through').attr("contentEditable", false);
-      button.removeClass(unchecked + ' mark-alt').addClass('pop_in').addClass(checked);
-      check_count += 1;
-    }
-    else
-    {    
-      linethrough_text.removeClass('line-through').attr('contentEditable', true);
-      button.removeClass(checked).removeClass('pop_in').addClass(unchecked + ' mark-alt');
-      check_count -= 1;
-    }
-    
-    updateText();
-  });
-  // Click on button function and delete 'li'
-  $('.app ul').on('click','li .delete_button',function(e){
-    e.preventDefault();
-    let index_click = $('li .delete_button').index(this);
-    let current = $('li').eq(index_click);
-    let button = $('.check_button').find('i');
-    
-    $(this).prop("disabled", true);
-    total -= 1;
-    
-    if(button.eq(index_click).hasClass('mark'))
-    {
-      check_count -= 1;
-    }
-    
-    current.addClass('up');
-    setTimeout(function () 
-    {
-      current.remove();  
-    }, 560); 
-    
-    $('#undo').removeClass('pop_out').addClass('pop_in').prop('disabled', false);
-    
-    updateText();
-  });
-  
-  showDate();
-  loadLi();
-});
+    var ctrlDeleteItem = function(e) {
+        var todo = e.target.parentNode.parentNode.parentNode;
+        if (e.target && e.target.className === "ion-ios-trash-outline") {
+            todo.parentNode.removeChild(todo);
+            listCtrl.deleteItem();
+        }
+        UICtrl.displayTotalItems();
+    };
+    return {
+        init: function() {
+            UICtrl.displayTotalItems();
+            UICtrl.displayDate();
+            setupEventListeners(); 
+        },
+    };
+})(listController, UIController);
+controller.init();
